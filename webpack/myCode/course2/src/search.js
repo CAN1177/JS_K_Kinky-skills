2b7b0f9aef0d1bc16921431d828997dabd5790a8
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-10-14 23:05:15 +0800

    feat: css解析、less解析

--- webpack/myCode/course2/src/search.js
+++ webpack/myCode/course2/src/search.js
@@ -4,6 +4,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import './search.less'
 
 class Search extends React.Component {
   render() {
--- webpack/myCode/course2/webpack.config.js
+++ webpack/myCode/course2/webpack.config.js
@@ -9,22 +9,33 @@ module.exports = {
   //   filename: "bundle.js"
   // },
 
-  // 多入口配置  
+  // 多入口配置
   entry: {
     index: "./src/index.js",
     search: "./src/search.js",
   },
   output: {
-    filename: '[name].js',
-    path:  path.join(__dirname,"dist"),
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
   },
   mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: "babel-loader",
+      },
+      //css 解析
+      {
+        test: /\.css$/,
+        // 注意这里是执行是从又到左
+        use: ["style-loader", "css-loader"],
+      },
+      //less 解析
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader",]
       }
-    ]
-  }
+    ],
+  },
 };
