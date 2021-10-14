11958081e3dd49b99a526c784506cf085413166c
CAN1177 <18209463505@163.com>
2021-10-14 17:30:33 +0800

    feat: 解析es6\react

---
+++ webpack/myCode/course2/webpack.config.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  // entry: "./src/index.js",
+  // output: {
+  //   path: path.join(__dirname,"dist"),
+  //   filename: "bundle.js"
+  // },
+
+  // 多入口配置  
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js",
+  },
+  output: {
+    filename: '[name].js',
+    path:  path.join(__dirname,"dist"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
