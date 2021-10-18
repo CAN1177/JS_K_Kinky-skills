579560434e4e69c6cc7a71d1378ab4668e7c1553
CAN1177 <18209463505@163.com>
2021-10-18 14:23:35 +0800

    feat: webpack-dev-serve

---
+++ B站webpack/myCode/2、webpack-dev-server/dist/bundle.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("console.log('下次一定5555');\nconsole.log('下次一定');\n\n//# sourceURL=webpack://1_loaders/./src/index.js?");
+/******/ })()
+;
---
+++ B站webpack/myCode/2、webpack-dev-server/src/index.js
@@ -0,0 +1,2 @@
+console.log('下次一定5555');
+console.log('下次一定');
